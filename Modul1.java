4aff7ddf53f467cd5ec4e86b2609ad9cec5dcd69
SilvyanaSalsabila14 <132529658+SilvyanaSalsabila14@users.noreply.github.com>
2024-08-22 14:17:17 +0700

    Add files via upload

---
+++ Modul1.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modul.pkg1;
+
+/**
+ *
+ * @author ThinkPad
+ */
+public class hello_dunia
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        {
+        System.out.println( "Nama : silvyana Salsabila");
+        System.out.println( "Gender : Perempuan");
+        System.out.println( "Alamat : Muara babadak");
+        System.out.println( "Tanggal Lahir : 14 september 2007");
+    }
+    }
+    public class cek_literal
+    {
+        public static void main(string[] args) }
+   {
+       //data variable dan jenis tipe datanya
+       long c=41;
+       doub
+               }
+}
+
+            
+        }
+    }
+    
+}
